d212713d9d0e6ec0df53d02b688cc03518c5bf81
Ponlawat <ponlawat_w@cmu.ac.th>
2018-01-15 23:15:39 +0700

    fixed block review css layout

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018011500;
+$plugin->version   = 2018011501;
 $plugin->release   = '2.3, release 3';
 $plugin->requires  = 2012062500.00; // Moodle 2.3.0
 $plugin->component = 'block_majhub';
